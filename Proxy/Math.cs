f166ffdeae4b420ca008f32c7208fc59b4692d24
Cezary Gorka <cgorka@live.com>
2016-11-11 22:58:07 +0100

    PROXY

---
+++ Proxy/Math.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public interface IMath
+    {
+        int Add(int x, int y);
+        int Sub(int x, int y);
+    }
+
+    public class Math:IMath
+    {
+        public int Add(int x, int y)
+        {
+            return x+y;
+        }
+
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    public class MathProxy : IMath
+    {
+        private  Math _math = new Math();
+        public int Add(int x, int y)
+        {
+            return _math.Add(x, y);
+        }
+
+        public int Sub(int x, int y)
+        {
+            return _math.Sub(x, y);
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathProxy proxy = new MathProxy();
+
+            // Do the math
+            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
